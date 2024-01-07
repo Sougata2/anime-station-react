91a3de9948ee5abb21b37d57d283c70150bed4bc
sougata <sougata19582001@gmail.com>
2024-01-08 01:14:11 +0530

    added anime tab links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+const urls = {
+  recent_episodes_url: "https://api.jikan.moe/v4/watch/episodes",
+  anime_genres_url: "https://myanimelist.net/anime/genre/1/Action",
+  anime_recomendations_url: "https://api.jikan.moe/v4/recommendations/anime",
+  top_anime_url: "https://api.jikan.moe/v4/top/anime",
+  season_now_anime: "https://api.jikan.moe/v4/seasons/now",
+  season_anime: "https://api.jikan.moe/v4/seasons/{year}/{season}",
+  random_anime: "https://api.jikan.moe/v4/random/anime",
+};
 
 function App() {
-  return (
-    <div>Hello React</div>
-  );
+  return <div>Hello React</div>;
 }
 
 export default App;
