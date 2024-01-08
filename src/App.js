cc8dadfc5212c4412fd3a8a9a7916f3479d30e02
sougata das <sougata19582001@gmail.com>
2024-01-09 01:01:16 +0530

    recent episode page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 const urls = {
   recent_episodes_url: "https://api.jikan.moe/v4/watch/episodes",
@@ -13,6 +14,7 @@ function App() {
   return (
     <div>
       <Header />
+      <AnimeList />
     </div>
   );
 }
@@ -23,10 +25,10 @@ function Header() {
       <nav
         className="navbar navbar-expand-lg"
         data-bs-theme="dark"
-        style={{ backgroundColor: "#712cf9"}}
+        style={{ backgroundColor: "#712cf9" }}
       >
         <div className="container">
-          <a className="navbar-brand" href="">
+          <a className="navbar-brand" href="/">
             Anime Station
           </a>
           <button
@@ -43,19 +45,19 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="/">
                   Link
                 </a>
               </li>
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
-                  href="#"
+                  href="/"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
@@ -64,12 +66,12 @@ function Header() {
                 </a>
                 <ul className="dropdown-menu">
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="/">
                       Action
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="/">
                       Another action
                     </a>
                   </li>
@@ -77,16 +79,11 @@ function Header() {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="/">
                       Something else here
                     </a>
                   </li>
                 </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </a>
               </li>
             </ul>
           </div>
@@ -94,6 +91,79 @@ function Header() {
       </nav>
     </header>
   );
+}
+
+function AnimeCard({ image_url, title, url }) {
+  return (
+    <div className="col" style={{ margin: "0", padding:"2rem" }}>
+      <div className="card mx-auto" style={{ width: "18rem", height: "35rem" }}>
+        <img src={image_url} className="card-img-top" alt="..." style={{width: "100%", height:"25rem"}}/>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text"></p>
+          <a
+            href={url}
+            className="btn btn-primary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Know More &#10138;
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+class AnimeList extends React.Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch(urls.recent_episodes_url)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true,
+        });
+      });
+  }
+  render() {
+    const {
+      DataisLoaded,
+      items: { data, pagination },
+    } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1> Pleses wait some time.... </h1>
+        </div>
+      );
+
+    return (
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-2">
+          {data.map((anime) => (
+            <AnimeCard
+              key={anime.entry.mal_id}
+              image_url={anime.entry.images.jpg.image_url}
+              title={anime.entry.title}
+              url={anime.entry.url}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
