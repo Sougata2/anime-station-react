fb48dbe43045a8dd44e5efd980f5e66bcc7ea1a3
sougata <sougata19582001@gmail.com>
2024-01-15 02:00:06 +0530

    one card style and navigation

--- src/getSeasonNow.js
+++ src/getSeasonNow.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const url = "https://api.jikan.moe/v4/seasons/now";
+let url = "https://api.jikan.moe/v4/seasons/now?page={%PAGE%}&&limit=1";
 
 export default class GetSeasonNow extends React.Component {
   constructor(props) {
@@ -8,11 +8,12 @@ export default class GetSeasonNow extends React.Component {
     this.state = {
       items: [],
       DataisLoaded: false,
+      page: 1,
     };
   }
 
   componentDidMount() {
-    fetch(url)
+    fetch(url.replace("{%PAGE%}", this.state.page))
       .then((res) => res.json())
       .then((json) => {
         this.setState({
@@ -21,11 +22,28 @@ export default class GetSeasonNow extends React.Component {
         });
       });
   }
+  goToNext() {
+    if (this.state.items.pagination.has_next_page)
+      this.setState({
+        page:
+          this.state.page === this.state.items.pagination.last_visible_page
+            ? 1
+            : this.state.page + 1,
+      });
+    this.componentDidMount();
+  }
+  goToPrevious() {
+    if (this.state.items.pagination.current_page === 1)
+      this.setState({ page: this.state.items.pagination.last_visible_page });
+    else this.setState({ page: this.state.page - 1 });
+    this.componentDidMount();
+  }
   render() {
     const {
       items: { data, pagination },
       DataisLoaded,
     } = this.state;
+    // const newData = data.slice(1, data.length);
     if (!DataisLoaded) {
       return (
         <div className="section-start">
@@ -33,12 +51,29 @@ export default class GetSeasonNow extends React.Component {
         </div>
       );
     }
+
     return (
       <div className="container-fluid card-list section-start">
         <div className="row row-cols-1 row-cols-md-2">
-          <h1>Season Now</h1>
+          {data.map((anime) => (
+            <AnimeCard anime={anime} />
+          ))}
         </div>
+        <button className="btn btn-primary" onClick={() => this.goToPrevious()}>
+          prev
+        </button>
+        <button className="btn btn-primary" onClick={() => this.goToNext()}>
+          Next
+        </button>
       </div>
     );
   }
+}
+
+function AnimeCard({ anime }) {
+  return (
+    <div className="col">
+      <img src={anime.images.jpg.image_url} alt="..." />
+    </div>
+  );
 }
