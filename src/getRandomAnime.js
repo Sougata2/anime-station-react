1f68fa4ed10ce5813c5714f3226b13a99cb315c8
sougata <sougata19582001@gmail.com>
2024-01-29 02:06:49 +0530

    added Random anime card

--- src/getRandomAnime.js
+++ src/getRandomAnime.js
@@ -7,6 +7,7 @@ export default class GetRandomAnime extends React.Component {
     this.state = {
       items: [],
       DataisLoaded: false,
+      refresh: false,
     };
   }
 
@@ -20,6 +21,20 @@ export default class GetRandomAnime extends React.Component {
         });
       });
   }
+
+  componentDidUpdate(prevProp, prevState) {
+    if (this.state.refresh !== prevState.refresh) {
+      fetch(url)
+        .then((res) => res.json())
+        .then((json) => {
+          this.setState({
+            items: json,
+            DataisLoaded: true,
+          });
+        });
+    }
+  }
+
   render() {
     const {
       items: { data },
@@ -28,16 +43,128 @@ export default class GetRandomAnime extends React.Component {
     if (!DataisLoaded) {
       return (
         <div className="section-start">
-          <h1> Pleses wait some time.... </h1>
+          <h1> Please wait some time.... </h1>
         </div>
       );
     }
     return (
-      <div className="container-fluid card-list section-start">
-        <div className="row row-cols-1 row-cols-md-2">
-          <h1>Random Anime</h1>
+      <div
+        className="container-fluid card-list section-start"
+        style={{ padding: "1rem" }}
+      >
+        <div className="row row-cols-1 row-cols-md-1">
+          <AnimeCard anime={data} />
+        </div>
+        <div className="text-center">
+          <i
+            className="fa-solid fa-rotate-right page-btn"
+            onClick={() =>
+              this.setState((prevState) => ({ refresh: !prevState.refresh }))
+            }
+          ></i>
         </div>
       </div>
     );
   }
+}
+
+/////////////////////////////////////////////////////
+///Using useEffect function with async and await.///
+///////////////////////////////////////////////////
+// export default function GetRandomAnime() {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [random_anime, setRandomAnime] = useState([]);
+
+//   useEffect(() => {
+//     async function getResponse() {
+//       setIsLoading(true);
+//       const anime = await fetch(url).then((res) => res.json());
+//       setRandomAnime(() => anime.data);
+//       setIsLoading(false);
+//     }
+//     getResponse();
+//   }, []);
+
+//   return (
+//     <div
+//       className="container-fluid card-list section-start"
+//       style={{ padding: "1rem" }}
+//     >
+//       {isLoading ? "Please Wait!" : <AnimeCard anime={random_anime} />}
+//     </div>
+//   );
+// }
+
+function AnimeCard({
+  anime: {
+    aired,
+    airing,
+    broadcast,
+    duration,
+    episodes,
+    genres,
+    images,
+    rating,
+    season,
+    status,
+    synopsis,
+    title,
+    trailer,
+    type,
+    url,
+  },
+}) {
+  const next_episode = new Date(aired.from);
+  return (
+    <div className="col">
+      <div className="anime-card mx-auto">
+        <div className="anime-card-left">
+          <img src={images.jpg.image_url} alt="" className="anime-card-image" />
+          <a
+            href={url}
+            className="anime-card-button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go
+          </a>
+        </div>
+        <div className="anime-card-right">
+          <div className="anime-card-title">{title}</div>
+          <div className="anime-card-div">
+            <span className="anime-card-div-key">Status : </span>
+            {status}
+          </div>
+          {/* <div className="anime-card-div"><span>Aired : </span>{aired.string}</div> */}
+          {/* <div className="anime-card-div">
+            <span>Aired : </span>
+            {aired.string}
+          </div> */}
+          {airing ?? (
+            <div className="anime-card-div">
+              <span>Next Episode : </span>
+              {broadcast.day}, {next_episode.getHours()}:
+              {next_episode.getMinutes()}{" "}
+              {next_episode.getHours() >= 12 ? "PM" : "AM"}
+            </div>
+          )}
+          <div className="anime-card-div">
+            <span>Duration : </span> {duration}
+          </div>
+          <div className="anime-card-div">
+            <span>Episodes : </span>
+            {episodes}
+          </div>
+          <div className="anime-card-div">
+            <span>Generes : </span>
+            {genres.map((genre) => (
+              <i className="" key={genre.mal_id}>
+                {genre.name},{" "}
+              </i>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
