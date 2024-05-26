6b3ba19e955414fac3d933ff38c9682882dc0ad3
sougata <sougata19582001@gmail.com>
2024-05-27 02:11:39 +0530

    favourite anime settings

--- src/ui/card/Favourite.jsx
+++ src/ui/card/Favourite.jsx
@@ -1,24 +1,40 @@
 import styled from "styled-components";
-import { addToFavourite } from "../../services/favouritesApi";
+import { addToFavourite, isFavourite } from "../../services/favouritesApi";
+import { useEffect, useRef, useState } from "react";
 
 const StyledFavourite = styled.div`
   position: absolute;
   right: 20px;
   bottom: 19px;
   font-size: 25px;
-  color: #3333;
+  color: ${({ $isFav }) => ($isFav ? "#03030373" : "#3333")};
   &:hover {
     color: #03030373;
   }
 `;
 
 function Favourite({ anime }) {
-  function handleClick() {
-    console.log(anime);
-    addToFavourite(anime);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(function () {
+    async function checkForFavourite() {
+      const res = await isFavourite(anime.anilistId);
+      setIsDisabled(res);
+    }
+    checkForFavourite();
+  });
+
+  async function handleClick() {
+    if (isDisabled) alert("Already Added To Favourites!");
+    ref.current.style.color = "#03030373";
+    setIsDisabled(true);
+
+    const { id, name, poster, anilistId } = anime;
+    addToFavourite({ id, name, poster, anilistId });
   }
   return (
-    <StyledFavourite onClick={handleClick}>
+    <StyledFavourite onClick={handleClick} $isFav={isDisabled} ref={ref}>
       <i className="fa-solid fa-heart"></i>
     </StyledFavourite>
   );
