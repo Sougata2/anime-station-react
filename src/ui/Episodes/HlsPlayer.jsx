83931d7d0520fe4e780634b40ee0a39bc6ea7b84
sougata <sougata19582001@gmail.com>
2024-06-25 17:46:20 +0530

    custom skip buttons

--- src/ui/Episodes/HlsPlayer.jsx
+++ src/ui/Episodes/HlsPlayer.jsx
@@ -1,27 +1,63 @@
-import Hls from "hls.js";
-import React, { useRef, useEffect } from "react";
 import "@videojs/themes/dist/city/index.css";
 
-import styled from "styled-components";
+import React, { useRef, useEffect } from "react";
+
 import videojs from "video.js";
+import styled from "styled-components";
+import Hls from "hls.js";
 
 const Video = styled.video`
   height: 100%;
   width: 100%;
+  position: relative;
+`;
+
+const SkipButtonForward = styled.div`
+  position: absolute;
+  z-index: 10;
+  right: 0;
+  width: 40%;
+  height: 65%;
+  top: 12%;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & svg {
+    visibility: hidden;
+    stroke-width: 4px;
+  }
+`;
+const SkipButtonBackward = styled.div`
+  position: absolute;
+  z-index: 10;
+  left: 0;
+  height: 65%;
+  top: 12%;
+  width: 40%;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & svg {
+    visibility: hidden;
+    stroke-width: 4px;
+  }
 `;
 
 function HLSPlayer({ url, trks }) {
   const videoRef = useRef(null);
+  const forwardRef = useRef(null);
+  const backwardRef = useRef(null);
+  const forwardSvgRef = useRef(null);
+  const backwardSvgRef = useRef(null);
+
   useEffect(
     function () {
       videojs(
         videoRef.current,
         {
           controlBar: {
-            skipButtons: {
-              backward: 10,
-              forward: 10,
-            },
             pictureInPictureToggle: false,
           },
           controls: true,
@@ -31,6 +67,21 @@ function HLSPlayer({ url, trks }) {
           const hls = new Hls();
           hls.loadSource(url);
           hls.attachMedia(videoRef.current);
+          const player = this.player_;
+          forwardRef.current.addEventListener("dblclick", function (e) {
+            player.currentTime(player.currentTime() + 10);
+            forwardSvgRef.current.style.visibility = "visible";
+            setTimeout(() => {
+              forwardSvgRef.current.style.visibility = "hidden";
+            }, 500);
+          });
+          backwardRef.current.addEventListener("dblclick", function () {
+            player.currentTime(player.currentTime() - 10);
+            backwardSvgRef.current.style.visibility = "visible";
+            setTimeout(() => {
+              backwardSvgRef.current.style.visibility = "hidden";
+            }, 500);
+          });
         }
       );
     },
@@ -39,6 +90,62 @@ function HLSPlayer({ url, trks }) {
 
   return (
     <>
+      <SkipButtonBackward ref={backwardRef}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          strokeWidth="2.5"
+          stroke="#ffffff"
+          fill="none"
+          ref={backwardSvgRef}
+          className="duration-300 transform transition-all"
+          style={{ width: "20%", height: "20%" }}
+        >
+          <path
+            strokeLinecap="round"
+            d="M9.57 15.41l2.6 8.64 8.64-2.61M26.93 41.41V23a.09.09 0 00-.16-.07s-2.58 3.69-4.17 4.78"
+          ></path>
+          <rect
+            x="32.19"
+            y="22.52"
+            width="11.41"
+            height="18.89"
+            rx="5.7"
+          ></rect>
+          <path
+            d="M12.14 23.94a21.91 21.91 0 11-.91 13.25"
+            strokeLinecap="round"
+          ></path>
+        </svg>
+      </SkipButtonBackward>
+      <SkipButtonForward ref={forwardRef}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 64 64"
+          strokeWidth="2.5"
+          stroke="#ffffff"
+          fill="none"
+          ref={forwardSvgRef}
+          className="duration-300 transform transition-all"
+          style={{ width: "20%", height: "20%" }}
+        >
+          <path
+            d="M23.93 41.41V23a.09.09 0 00-.16-.07s-2.58 3.69-4.17 4.78"
+            strokeLinecap="round"
+          ></path>
+          <rect
+            x="29.19"
+            y="22.52"
+            width="11.41"
+            height="18.89"
+            rx="5.7"
+          ></rect>
+          <path
+            strokeLinecap="round"
+            d="M54.43 15.41l-2.6 8.64-8.64-2.61M51.86 23.94a21.91 21.91 0 10.91 13.25"
+          ></path>
+        </svg>
+      </SkipButtonForward>
       <Video
         ref={videoRef}
         id="my-video"
--- src/ui/Episodes/VideoPlayer.jsx
+++ src/ui/Episodes/VideoPlayer.jsx
@@ -3,8 +3,10 @@ import HLSPlayer from "./HlsPlayer";
 import useVideo from "../../features/Episodes/useVideo";
 import styled from "styled-components";
 import EpisodeLoader from "./EpisodeLoader";
+import { useRef, useState } from "react";
 
 const PlayerContainer = styled.div`
+  position: relative;
   display: inline-block;
   border-radius: 10px;
   overflow: hidden;
@@ -16,14 +18,42 @@ const PlayerContainer = styled.div`
   }
 `;
 
+const ExpandButton = styled.div`
+  cursor: pointer;
+  color: white;
+  position: absolute;
+  z-index: 11;
+  right: 10px;
+  top: 10px;
+`;
+
 function VideoPlayer() {
+  const [fullscreen, setFullscreen] = useState(false);
+  const windowRef = useRef(null);
   const { isPending, isRefetching, data } = useVideo();
+
+  function handleFullscreen() {
+    if (fullscreen) {
+      setFullscreen(false);
+      document.exitFullscreen();
+    } else {
+      setFullscreen(true);
+      windowRef.current.requestFullscreen();
+    }
+  }
 
   if (isPending || isRefetching) return <EpisodeLoader />;
 
   return (
     <>
-      <PlayerContainer>
+      <PlayerContainer ref={windowRef}>
+        <ExpandButton className="full-screen" onClick={handleFullscreen}>
+          {fullscreen ? (
+            <i className="fas fa-compress-alt"></i>
+          ) : (
+            <i className="fa-solid fa-up-right-and-down-left-from-center"></i>
+          )}
+        </ExpandButton>
         {Object.keys(data).includes("status") ? (
           <ErrorVideo />
         ) : (
