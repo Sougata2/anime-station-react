3c34e4db14cc972f3112b7e882fe25be270415b1
sougata <sougata19582001@gmail.com>
2024-07-01 00:31:17 +0530

    Header box shadow

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -13,6 +13,8 @@ const NavBar = styled.div`
   background-color: #e384ff;
   color: white;
   padding: 0 2rem;
+  box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px,
+    rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
   @media (max-width: 800px) {
     display: flex;
     flex-direction: column;
