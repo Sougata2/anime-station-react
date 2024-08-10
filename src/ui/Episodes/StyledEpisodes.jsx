fe4cf833f8573483536ac5aa10d3ce6d51b87894
sougata <sougata19582001@gmail.com>
2024-08-10 23:52:42 +0530

    height of the episodes box fixed

--- src/ui/Episodes/StyledEpisodes.jsx
+++ src/ui/Episodes/StyledEpisodes.jsx
@@ -4,8 +4,9 @@ const Div = styled.div`
     display: grid;
     grid-template-rows: 500px 100px auto;
     padding-top: 5px;
+    height: 100vh;
     @media (max-width: 800px) {
-        grid-template-rows: 280px 1fr 250px;
+        grid-template-rows: 280px 1fr auto;
     }
 `;
 function StyledEpisodes({ children }) {
