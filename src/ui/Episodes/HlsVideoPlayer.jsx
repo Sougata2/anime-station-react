5467a8a72be9308b1ec0d1938a6f5f3974701058
sougata <sougata19582001@gmail.com>
2024-12-13 22:44:05 +0530

    error message handler for video error

--- src/ui/Episodes/HlsVideoPlayer.jsx
+++ src/ui/Episodes/HlsVideoPlayer.jsx
@@ -1,43 +1,48 @@
-import React, { useEffect, useRef } from "react";
+import React, {useEffect, useRef} from "react";
 import Hls from "hls.js";
 import styled from "styled-components";
+import toast from "react-hot-toast";
 
 const StyledVideo = styled.video`
-  background-color: #333;
+    background-color: #333;
 `;
 
-function HlsVideoPlayer({ url, tracks }) {
-  const player = useRef(null);
-  useEffect(
-    function () {
-      const hls = new Hls();
-      hls.loadSource(url);
-      hls.attachMedia(player.current);
-    },
-    [url]
-  );
-  return (
-    <>
-      <StyledVideo
-        height={"100%"}
-        width={"100%"}
-        crossOrigin="anonymous"
-        ref={player}
-        controls
-      >
-        {tracks?.map((track, i) => (
-          <track
-            key={i}
-            src={track.file}
-            kind={track.kind === "captions" ? "subtitles" : track.kind}
-            label={track.label?.split(" ").at(0)}
-            srcLang={track.label?.toLocaleLowerCase().slice(0, 2)}
-            default={track.default ? true : false}
-          />
-        ))}
-      </StyledVideo>
-    </>
-  );
+function HlsVideoPlayer({url, tracks}) {
+    const player = useRef(null);
+    useEffect(
+        function () {
+            try {
+                const hls = new Hls();
+                hls.loadSource(url);
+                hls.attachMedia(player.current);
+            } catch (e) {
+                toast.error(e.message);
+            }
+        },
+        [url]
+    );
+    return (
+        <>
+            <StyledVideo
+                height={"100%"}
+                width={"100%"}
+                crossOrigin="anonymous"
+                ref={player}
+                controls
+            >
+                {tracks?.map((track, i) => (
+                    <track
+                        key={i}
+                        src={track.file}
+                        kind={track.kind === "captions" ? "subtitles" : track.kind}
+                        label={track.label?.split(" ").at(0)}
+                        srcLang={track.label?.toLocaleLowerCase().slice(0, 2)}
+                        default={track.default ? true : false}
+                    />
+                ))}
+            </StyledVideo>
+        </>
+    );
 }
 
 export default HlsVideoPlayer;
