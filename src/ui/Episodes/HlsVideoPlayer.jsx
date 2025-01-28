beb1b20cc148c877f80b0c350104a6c480dc89b1
sougata <sougata19582001@gmail.com>
2025-01-28 17:24:30 +0530

    fixed subtitle not appearing by default

--- src/ui/Episodes/HlsVideoPlayer.jsx
+++ src/ui/Episodes/HlsVideoPlayer.jsx
@@ -1,48 +1,59 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import Hls from "hls.js";
 import styled from "styled-components";
 import toast from "react-hot-toast";
 
 const StyledVideo = styled.video`
-    background-color: #333;
+  background-color: #333;
 `;
 
-function HlsVideoPlayer({url, tracks}) {
-    const player = useRef(null);
-    useEffect(
-        function () {
-            try {
-                const hls = new Hls();
-                hls.loadSource(url);
-                hls.attachMedia(player.current);
-            } catch (e) {
-                toast.error(e.message);
-            }
-        },
-        [url]
-    );
-    return (
-        <>
-            <StyledVideo
-                height={"100%"}
-                width={"100%"}
-                crossOrigin="anonymous"
-                ref={player}
-                controls
-            >
-                {tracks?.map((track, i) => (
-                    <track
-                        key={i}
-                        src={track.file}
-                        kind={track.kind === "captions" ? "subtitles" : track.kind}
-                        label={track.label?.split(" ").at(0)}
-                        srcLang={track.label?.toLocaleLowerCase().slice(0, 2)}
-                        default={track.default ? true : false}
-                    />
-                ))}
-            </StyledVideo>
-        </>
-    );
+function HlsVideoPlayer({ url, tracks }) {
+  const player = useRef(null);
+  useEffect(
+    function () {
+      try {
+        const hls = new Hls();
+        hls.loadSource(url);
+        hls.attachMedia(player.current);
+      } catch (e) {
+        toast.error(e.message);
+      }
+    },
+    [url]
+  );
+
+  useEffect(() => {
+    const videoElement = player.current;
+    if (videoElement) {
+      const textTracks = videoElement.textTracks;
+      Array.from(textTracks).forEach((track, index) => {
+        track.mode = tracks[index]?.default ? "showing" : "disabled";
+      });
+    }
+  }, [tracks]);
+
+  return (
+    <>
+      <StyledVideo
+        height={"100%"}
+        width={"100%"}
+        crossOrigin="anonymous"
+        ref={player}
+        controls
+      >
+        {tracks.map((track, i) => (
+          <track
+            key={i}
+            src={track.file}
+            kind={track.kind === "captions" ? "subtitles" : track.kind}
+            label={track.label?.split(" ").at(0)}
+            srcLang={track.label?.toLocaleLowerCase().slice(0, 2)}
+            default={track.default}
+          />
+        ))}
+      </StyledVideo>
+    </>
+  );
 }
 
 export default HlsVideoPlayer;
